dc3143370cb16577c8b275adacc4b16f5d934df5
Neha <shahneha403@gmail.com>
2018-10-15 13:36:50 +0400

    Adding the webdrivers for Chrome and Firefox, for both mac and windows

--- selenium_framework_olx/src/selenium_framework_olx/sampleSeleniumTest.java
+++ selenium_framework_olx/src/selenium_framework_olx/sampleSeleniumTest.java
@@ -1,5 +1,23 @@
 package selenium_framework_olx;
 
-public class sampleSeleniumTest {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
+public class sampleSeleniumTest {
+	
+	public static void main(String[] args) throws InterruptedException{
+
+	//Create Driver Object
+	
+	     WebDriver driver = new SafariDriver();
+	     
+	     driver.get("https://dubai.dubizzle.com");
+	     
+	    
+	     
+	     
+	}    
+	     
+	     
 }
